93af8fa3771d6eed66d92956ae69c2ca3b0ced4b
vallirathi <drvallirathi@gmail.com>
2026-03-17 13:22:50 +0530

    Update CODE.js

--- CODE.js
+++ CODE.js
@@ -6,5 +6,5 @@ let number2 = 20;
 let sum = number1 + number2;
 
 // Display the result in the console
-console.log("The sum is: " + sum);
+console.log("The sumyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy is: " + sum);
 // Output: The sum is: 30
